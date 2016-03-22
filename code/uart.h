72e120f7f5378acd2c2eb12da82cd50c6f706c79
Jason R. Jones <jjones@updsys.com>
2016-03-22 16:09:27 -0400

    Removed some space in file

--- code/uart.h
+++ code/uart.h
@@ -3,15 +3,12 @@
 
 #include <stdint.h>
 
-
-
 void UART_init(void);
 
 int32_t UART_read(uint8_t* data, uint32_t length);
 int32_t UART_write(uint8_t* data, uint32_t length);
 int32_t UART_readable(void);
 int32_t UART_writeable(void);
-
 
 #endif
 
