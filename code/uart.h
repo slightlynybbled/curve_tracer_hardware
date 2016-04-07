2c36fad5f784a2247db286582b376c6328fc0c7c
Jason R. Jones <jjones@updsys.com>
2016-04-07 10:24:58 -0400

    Added documentation

--- code/uart.h
+++ code/uart.h
@@ -6,11 +6,39 @@
 #define TX_BUF_LENGTH       128
 #define RX_BUF_LENGTH       32
 
+/**
+ * Initializes the UART
+ */
 void UART_init(void);
 
+/**
+ * Reads data from the UART receive circular buffer
+ * 
+ * @param data destination array pointer
+ * @param length number of bytes to read
+ */
 void UART_read(void* data, uint32_t length);
+
+/**
+ * Writes data to the UART send circular buffer
+ * 
+ * @param data source array pointer of the data to write
+ * @param length length of the data to write
+ */
 void UART_write(void* data, uint32_t length);
+
+/**
+ * Returns the number of bytes waiting to be read
+ * from the UART circuilar buffer
+ * 
+ * @return length of the data
+ */
 int16_t UART_readable(void);
+
+/**
+ * Returns the number of bytes that can be written
+ * @return the number of bytes that can be written
+ */
 int16_t UART_writeable(void);
 
 #endif
