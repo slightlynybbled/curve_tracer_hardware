765a48455cc736063049bd3ea2b2643414799b7d
Jason R. Jones <jjones@updsys.com>
2016-04-07 09:56:57 -0400

    Added docstrings to frame

--- code/frame.h
+++ code/frame.h
@@ -4,8 +4,25 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+/**
+ * Must be called before any other framing operations.
+ */
 void FRM_init(void);
+
+/**
+ * Use to push an array element to be framed and sent.
+ * @param data a byte-aligned array pointer
+ * @param length the length of the array
+ */
 void FRM_push(uint8_t* data, uint16_t length);
+
+/**
+ * Use to read unframed data from the receive buffer
+ * 
+ * @param data a byte-aligned array pointer to which the
+ * unframed data will be copied
+ * @return length the length of the array
+ */
 uint16_t FRM_pull(uint8_t* data);
 
 #endif
